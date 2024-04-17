d749c817cc625c01a8c5229d9c815ecb8de4303f
Connor Findlay <git@findlays.io>
2024-04-17 19:37:17 +1200

    Initial demonstration.
    It's possible to side-step the JCE signer, by
    patching the JVM it runs in. This will probably
    stop working soon, as the Unsafe class is being
    retired - possibly to stop people doing this.

--- src/Runtime.java
+++ src/Runtime.java
@@ -8,6 +8,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.function.Function;
 
+/** Demonstrate JCE signing bypass.
+ *
+ */
 public class Runtime {
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, SignatureException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
         patchJCESigningOut();
--- src/jca/FindlayProvider.java
+++ src/jca/FindlayProvider.java
@@ -8,9 +8,24 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.Provider;
 
+/** Simple POC class
+ * <p>
+ * This class shows how one would go about implementing a CryptoProvider in the
+ * JCE/JCA.
+ *
+ * We demonstrate how simple it is to add a signature, cipher, and key generator.
+ *
+ * This class needs signed by SUN/IBM before it will be accepted by any JVM that
+ * they have produced.
+ *
+ * @author connor
+ */
 public class FindlayProvider extends Provider {
+    /** Name of this provider, used in getInstance calls */
     public static final String NAME = "FP";
+    /** Provider version, can be used to provide versioning */
     public static double VERSION = 1.0;
+    /** Information about this provider - normally something about why it exists */
     public static final String INFO = "Test Provider";
     /**
      * Constructs a provider with the specified name, version number,
@@ -22,16 +37,34 @@ public class FindlayProvider extends Provider {
      */
     protected FindlayProvider(String name, double version, String info) {
         super(name, version, info);
+        /* In a context where the Security Manager is awake and enforcing restrictions,
+         * this will allow us to install the custom implementations, provided the
+         * access rules allow it.
+         */
         AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
             installStuff();
             return null;
         });
     }
 
+    /**
+     * Constructs a provider with the default name, version number,
+     * and information.
+     *
+     *      * @param name    the provider name.
+     *      * @param version the provider version number.
+     *      * @param info    a description of the provider and its services.
+     */
     public FindlayProvider() {
         this(FindlayProvider.NAME,FindlayProvider.VERSION,FindlayProvider.INFO);
     }
 
+    /**
+     * Install our custom implementations into the JCA.
+     *
+     * We use my surname as the algorithm name, so that we are sure that we only have this
+     * class that provides the classes.
+     */
     private void installStuff(){
         //Should be OK
         put("Signature.Findlay", FindlaySignature.class.getName());
